04dab480c2aa38157e0ba7b3d8aa268712053aca
vinjn <vinjn.z@gmail.com>
2021-12-10 13:39:21 +0800

    Update

--- images-to-video.bat
+++ images-to-video.bat
@@ -1,6 +1,7 @@
+@REM images-to-video.bat c:\svn_pool\VQGAN-CLIP\steps\ c:\svn_pool\VQGAN-CLIP\samples\有一个人前来买瓜(原视频).mp4.mp3
 SET INPUT=c:\svn_pool\VQGAN-CLIP\steps\gua_%%4d.jpg
 SET AUDIO=%2
 SET OUTPUT=%1\output.mp4
-ffmpeg -i %AUDIO% -i %INPUT% -c:v libx264 -pix_fmt yuv420p %OUTPUT%
+ffmpeg -r 30 -i %INPUT% -i %AUDIO% -c:v libx264 -pix_fmt yuv420p  %OUTPUT%
 @REM ffmpeg -framerate 1 -pattern_type glob -i %INPUT%\*.jpg ^
 @REM   -c:v libx264 -r 30 -pix_fmt yuv420p %OUTPUT%
